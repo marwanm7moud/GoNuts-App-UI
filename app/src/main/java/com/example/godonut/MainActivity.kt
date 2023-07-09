21940e8473cc1dde1c5675ad1d56eb5e41faa5e8
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-07-09 20:14:41 +0200

    first commit

---
+++ app/src/main/java/com/example/godonut/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.godonut
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.godonut.screens.OnBoardingScreen
+import com.example.godonut.ui.theme.GoDonutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GoDonutTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    OnBoardingScreen()
+                }
+            }
+        }
+    }
+}
