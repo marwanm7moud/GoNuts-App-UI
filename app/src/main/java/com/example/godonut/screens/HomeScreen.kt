0f8d68206e1b8967d9c9c457816898176d47c738
Marwan Mahmoud <marwanmhmoudyassenn@gmail.com>
2023-07-12 15:53:31 +0200

    finish bottom nav

--- app/src/main/java/com/example/godonut/screens/HomeScreen.kt
+++ app/src/main/java/com/example/godonut/screens/HomeScreen.kt
@@ -13,10 +13,16 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -32,82 +38,120 @@ import com.example.godonut.ui.theme.Primary
 import com.example.godonut.ui.theme.Text100
 import com.example.godonut.ui.theme.Text60
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        item {
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(end = 40.dp)
-                    .padding(start = 38.dp, top = 81.dp)
+    Scaffold(
+        bottomBar = {
+            NavigationBar(
+                containerColor = Color.Transparent
             ) {
-                Column {
-                    Text(
-                        text = stringResource(id = R.string.letsGonuts),
-                        fontSize = 30.sp,
-                        fontWeight = FontWeight.SemiBold,
-                        color = Primary
-                    )
-                    Text(
-                        text = stringResource(id = R.string.subLetsGonuts),
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight.Medium,
-                        color = Text60
-                    )
-                }
-                Button(
-                    colors = ButtonDefaults.buttonColors(containerColor = Background),
-                    shape = RoundedCornerShape(15.dp),
-                    modifier = Modifier.size(45.dp),
-                    contentPadding = PaddingValues(0.dp),
-                    onClick = {}
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+
                 ) {
                     Icon(
-                        painter = painterResource(id = R.drawable.baseline_search_24),
-                        tint = Primary,
-                        contentDescription = "",
+                        painter = painterResource(id = R.drawable.home),
+                        contentDescription = ""
+                    )
+                    Icon(
+                        painter = painterResource(id = R.drawable.heart),
+                        contentDescription = ""
+                    )
+                    Icon(
+                        painter = painterResource(id = R.drawable.notification),
+                        contentDescription = ""
+                    )
+                    Icon(
+                        painter = painterResource(id = R.drawable.buy),
+                        contentDescription = ""
+                    )
+                    Icon(
+                        painter = painterResource(id = R.drawable.user),
+                        contentDescription = ""
                     )
                 }
             }
         }
-        item {
-            Text(
-                text = stringResource(id = R.string.todayOffers),
-                color = Text100,
-                modifier = Modifier.padding(start = 38.dp, top = 45.dp)
-            )
-        }
-        item {
-            LazyRow(
-                contentPadding = PaddingValues(horizontal = 38.dp),
-                modifier = Modifier.padding(top = 25.dp),
-                horizontalArrangement = Arrangement.spacedBy(45.dp)
-            ) {
-                items(5) {
-                    TodaysOfferCard()
+    ) { padding ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(padding)
+                .fillMaxSize()
+        ) {
+            item {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(end = 40.dp)
+                        .padding(start = 38.dp, top = 81.dp)
+                ) {
+                    Column {
+                        Text(
+                            text = stringResource(id = R.string.letsGonuts),
+                            fontSize = 30.sp,
+                            fontWeight = FontWeight.SemiBold,
+                            color = Primary
+                        )
+                        Text(
+                            text = stringResource(id = R.string.subLetsGonuts),
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight.Medium,
+                            color = Text60
+                        )
+                    }
+                    Button(
+                        colors = ButtonDefaults.buttonColors(containerColor = Background),
+                        shape = RoundedCornerShape(15.dp),
+                        modifier = Modifier.size(45.dp),
+                        contentPadding = PaddingValues(0.dp),
+                        onClick = {}
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.baseline_search_24),
+                            tint = Primary,
+                            contentDescription = "",
+                        )
+                    }
                 }
             }
-        }
-        item {
-            Text(
-                text = stringResource(id = R.string.donuts),
-                color = Text100,
-                modifier = Modifier.padding(start = 38.dp, top = 45.dp)
-            )
-        }
-        item {
-            LazyRow(
-                contentPadding = PaddingValues(horizontal = 38.dp),
-                modifier = Modifier.padding(top = 12.dp, bottom = 20.dp),
-                horizontalArrangement = Arrangement.spacedBy(20.dp)
-            ) {
-                items(5) {
-                    DonutsCard()
+            item {
+                Text(
+                    text = stringResource(id = R.string.todayOffers),
+                    color = Text100,
+                    modifier = Modifier.padding(start = 38.dp, top = 45.dp)
+                )
+            }
+            item {
+                LazyRow(
+                    contentPadding = PaddingValues(horizontal = 38.dp),
+                    modifier = Modifier.padding(top = 25.dp),
+                    horizontalArrangement = Arrangement.spacedBy(45.dp)
+                ) {
+                    items(5) {
+                        TodaysOfferCard()
+                    }
+                }
+            }
+            item {
+                Text(
+                    text = stringResource(id = R.string.donuts),
+                    color = Text100,
+                    modifier = Modifier.padding(start = 38.dp, top = 45.dp)
+                )
+            }
+            item {
+                LazyRow(
+                    contentPadding = PaddingValues(horizontal = 38.dp),
+                    modifier = Modifier.padding(top = 12.dp, bottom = 20.dp),
+                    horizontalArrangement = Arrangement.spacedBy(20.dp)
+                ) {
+                    items(5) {
+                        DonutsCard()
+                    }
                 }
             }
         }
